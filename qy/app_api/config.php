c54d3ca2ab1b583d9003e21fc17fb6f3239505bb
EngiGu <451292130@qq.com>
2017-04-17 12:36:36 +0800

    Create config.php

---
+++ qy/app_api/config.php
@@ -0,0 +1,17 @@
+<?php
+/* 本文件由系统自动生成 如非必要 请勿修改 */
+define("_host_","localhost");
+define("_user_","mysqlroot");
+define("_pass_","mysqlmima");
+define("_port_","3306");
+define("_ov_","ov");
+define("_openvpn_","openvpn");
+define("_iuser_","iuser");
+define("_ipass_","pass");
+define("_isent_","isent");
+define("_irecv_","irecv");
+define("_starttime_","starttime");
+define("_endtime_","endtime");
+define("_maxll_","maxll");
+define("_other_","dlid,tian");
+define("_i_","i");
